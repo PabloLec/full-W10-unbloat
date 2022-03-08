463760cf974f63b003ab5d6e3bdf379375182d4d
PabloLec <pablo.lecolinet@gmail.com>
2022-03-08 17:41:35 +0100

    Fix relative paths

--- unbloat.ps1
+++ unbloat.ps1
@@ -4,24 +4,27 @@ Set-Location $env:TEMP
 # W4RH4WK/Debloat-Windows-10
 Invoke-RestMethod -Uri https://github.com/W4RH4WK/Debloat-Windows-10/archive/refs/heads/master.zip -OutFile ./Debloat-Windows-10.zip
 Expand-Archive ./Debloat-Windows-10.zip
-Set-Location ./Debloat-Windows-10/scripts
+$scriptPath = Join-Path $env:TEMP "Debloat-Windows-10/Debloat-Windows-10-master/scripts"
+Set-Location $scriptPath
 ls -Recurse *.ps*1 | Unblock-File
-& .\block-telemetry.ps1
-& .\disable-services.ps1
-& .\fix-privacy-settings.ps1
-& .\optimize-user-interface.ps1
-& .\optimize-windows-update.ps1
-& .\remove-default-apps.ps1
-& .\remove-onedrive.ps1
+& Join-Path $scriptPath "block-telemetry.ps1"
+& Join-Path $scriptPath "disable-services.ps1"
+& Join-Path $scriptPath "fix-privacy-settings.ps1"
+& Join-Path $scriptPath "optimize-user-interface.ps1"
+& Join-Path $scriptPath "optimize-windows-update.ps1"
+& Join-Path $scriptPath "remove-default-apps.ps1"
+& Join-Path $scriptPath "remove-onedrive.ps1"
 
 Set-Location $env:TEMP
 # Sycnex/Windows10Debloater
 Invoke-RestMethod -Uri https://github.com/Sycnex/Windows10Debloater/archive/refs/heads/master.zip -OutFile ./Windows10Debloater.zip
 Expand-Archive ./Windows10Debloater.zip
-Set-Location ./Windows10Debloater
-& .\Windows10SysPrepDebloater.ps1 -Sysprep -Debloat -Privacy
+$scriptPath = Join-Path $env:TEMP "Windows10Debloater/Windows10Debloater-master"
+Set-Location $scriptPath
+$scriptFile = Join-Path $scriptPath "Windows10SysPrepDebloater.ps1"
+& $scriptFile -Sysprep -Debloat -Privacy
 
 Set-Location $env:TEMP
 # https://www.oo-software.com/en/shutup10
 Invoke-RestMethod -Uri https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe -OutFile ./OOSU10.exe
-Start-Process -Wait ./OOSU10.exe
+Start-Process ./OOSU10.exe
